47ab853e8dbf0dd986f68a89a5629ce72df45326
gyver98 <matthewchoi7@gmail.com>
2017-02-03 14:38:09 +1100

    install react-bootstrap-table lib

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ObjectData from './ObjectData';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>React Fixed Data Table</h2>
+        <div>
+          <ObjectData />
         </div>
-        <p className="App-intro">
-          Hello React!
-        </p>
-      </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './react-bootstrap-table-all.min.css';
+
+var products = [{
+      id: 1,
+      name: "Product1",
+      price: 120
+  }, {
+      id: 2,
+      name: "Product2",
+      price: 80
+  }, {
+      id: 3,
+      name: "Product3",
+      price: 180
+  }];
 
 ReactDOM.render(
-  <App />,
+  <BootstrapTable data={products} >
+      <TableHeaderColumn isKey dataField='id'>Product ID</TableHeaderColumn>
+      <TableHeaderColumn dataField='name'>Product Name</TableHeaderColumn>
+      <TableHeaderColumn dataField='price'>Product Price</TableHeaderColumn>
+  </BootstrapTable>,
   document.getElementById('root')
 );
